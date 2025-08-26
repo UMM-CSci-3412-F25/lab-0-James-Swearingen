34624da4ed953cf7dc426e60c7355af9005eb014
James Swearingen <swear051@morris.umn.edu>
2025-08-26 10:41:53 -0500

    fix shellcheck issues

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # extracts files from NthPrime into the current directory, making a new directory called NthPrime that contains the files to be compiled
 tar -xf NthPrime.tgz 
 # compiles both C files into a program NthPrime.out
 gcc NthPrime/main.c NthPrime/nth_prime.c -o NthPrime/NthPrime
 # runs the compiled program with the first argument that the script was called with
-NthPrime/NthPrime $1
+NthPrime/NthPrime "$1"
