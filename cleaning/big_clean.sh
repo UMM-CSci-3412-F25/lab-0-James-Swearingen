2a0d58be0e5735822daa2dcc8abd807a0822358f
James Swearingen <swear051@morris.umn.edu>
2025-08-26 11:26:16 -0500

    decompress into scratch and delete files
    currently the script hangs forever, am hoping to fix this next time I work on it

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#make a temp directory and remember its name
+scratch=$(mktemp -d)
+#remember where you came from
+here=$(pwd)
+#extract to the temp directory
+tar -xf "$1" -C "$scratch"
+#go into the scratch directory so we don't start scrubbing the entire system or something
+cd "$scratch"
+#by the power of grep (find every file that has the line "DELETE ME!" in it)
+#then pipes the output into xargs so they get deleted
+grep -l "DELETE ME!" | xargs -0 /bin/rm -f 
+
+
+
