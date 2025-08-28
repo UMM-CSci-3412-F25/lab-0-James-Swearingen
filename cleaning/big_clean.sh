195190809b8165307446f80ae59f0f52e8ec02d4
James Swearingen <swear051@morris.umn.edu>
2025-08-28 11:42:55 -0500

    generate tgz file that has too many files in it

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 #make a temp directory and remember its name
-scratch=$(mktemp -d)
+scratch="$(mktemp -d)"
+#remember the name of the file you are working with, without the extension, for use in directories
+filename="$(basename $1 .tgz)"
 #remember where you came from
 here=$(pwd)
+mkdir "$scratch/$filename"
 #extract to the temp directory
-tar -xf "$1" -C "$scratch"
+tar -xf "$1" -C "$scratch/$filename"
 #go into the scratch directory so we don't start scrubbing the entire system or something
 cd "$scratch"
 #by the power of grep (find every file that has the line "DELETE ME!" in it)
 #then pipes the output into xargs so they get deleted
 grep -lr "DELETE ME!" | xargs -0 rm -f 
+#create new tgz with the proper filename
+#creates the tgz with the name cleaned_<insert name of original file>
+#compressing all files from the directory that it is currently in 
+tar  -czf "cleaned_$filename.tgz" --exclude=".*\.tgz$" "$filename"
+mv "cleaned_$filename.tgz" "$here" 
+#return to whence ye came
+cd "$here"
 
 
 
